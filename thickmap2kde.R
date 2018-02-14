4534b9a24cba7caaff5a599a6b3ee5fcdff8f1d2
Matteo Abis <gmatteo..abis@gmail.com>
2018-02-14 10:19:56 +0100

    add kde outputs

--- thickmap2kde.R
+++ thickmap2kde.R
@@ -8,11 +8,11 @@ parser <- ArgumentParser(description='kde smoothing of thickness map distributio
 parser$add_argument('source', nargs=1)
 parser$add_argument('output', nargs=1)
 parser$add_argument('--density_bw', nargs='?', default=2, help='density bandwidth')
-parser$add_argument('--pixel_size', nargs='?', default=1.1, help='pixel size (um)')
+parser$add_argument('--pixel_size', nargs='?', default=0.6, help='pixel size (um)')
 parser$add_argument('--gridsize', nargs='?', default=512, help='kde gridsize')
 parser$add_argument('--bgridsize', nargs='?', default=512, help='kde bgridsize')
 parser$add_argument('--xmin', nargs='?', default=0, help='kde xmin')
-parser$add_argument('--xmax', nargs='?', default=150, help='kde xmax')
+parser$add_argument('--xmax', nargs='?', default=300, help='kde xmax')
 
 args <- parser$parse_args()
 dt = fread(paste("zcat", args$source))
